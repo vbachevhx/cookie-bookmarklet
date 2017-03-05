c99c4d37668b86ca0659c21821d58e0257c8a029
Vasil Bachev <vasil.bachev@holidayextras.com>
2017-03-05 23:54:42 +0200

    switching to production urls

--- src/js/config.js
+++ src/js/config.js
@@ -1,4 +1,7 @@
 module.exports = {
-	url: 'http://localhost:9966/dist/'
-	// url: 'https://rawgit.com/vbachevhx/cookie-bookmarklet/master/dist/'
+
+	// pick between the local development url or the rawgit hosted 'production' url
+
+	// url: 'http://localhost:9966/dist/'
+	url: 'https://rawgit.com/vbachevhx/cookie-bookmarklet/master/dist/'
 };
